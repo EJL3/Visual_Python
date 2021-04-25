3f540197081e282668b538555dd8d86bd5519356
Rizwan Abdul Rahim <rizwrahim@gmail.com>
2021-04-25 20:49:31 +0400

    VPython

--- Bouncing ball.py
+++ Bouncing ball.py
@@ -1,4 +1,5 @@
 from vpython import *
+
 run = False
 def runbutton(b):
     global run
@@ -6,11 +7,13 @@ def runbutton(b):
     else: b.text = 'Pause'
     run = not run
 button(text = "Run", color=color.blue, bind=runbutton)
-floor = box(pos=vector(0,-5,0), size=vector(10,0.2,10), texture=textures.wood)
-ball = sphere(pos=vector(-5,5,0), radius=0.6, color=color.blue, make_trail=True, emissive=True)
+
+floor = box(pos=vector(0, -5, 0), size=vector(10, 0.2, 10), texture=textures.wood)
+ball = sphere(pos=vector(-5, 5, 0), radius=0.6, color=color.blue, make_trail=True, emissive=True)
 
 ball.m = 0.5
-ball.p = ball.m*vector(1,4,0) #Projectile of the ball
+ball.p = ball.m*vector(1, 4, 0) #Projectile of the ball
+ball_weight = 0.8
 g = 9.8
 t = 0
 dt = 0.01
@@ -22,11 +25,11 @@ l = local_light(pos=ball.pos, color=ball.color)
 while ball.pos.x < 8:
     rate(200)
     if run:
-        f = vector(0, -ball.m*g,0)
-        ball.p = ball.p + f*dt
-        ball.pos = ball.pos + (ball.p/ball.m)*dt
+        f = vector(0, -ball.m*g, 0)
+        ball.p = ball.p + f * dt
+        ball.pos = ball.pos + (ball.p/ball.m) * dt
         l.pos = ball.pos
         if ball.pos.y < floor.pos.y + ball.radius:
-            ball.p.y = abs(ball.p.y)*.8 # The *.8 refers to the weight of ball
+            ball.p.y = abs(ball.p.y) * ball_weight
         t += dt
         y.plot(t, ball.p.y/ball.m)
